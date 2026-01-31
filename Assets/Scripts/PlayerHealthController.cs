044c76e49b3c9cc72d1a2e169645fce542f68b3d
FregsGames <69074198+FregsGames@users.noreply.github.com>
2026-01-31 11:00:18 +0000

    player health bar

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,37 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthController : MonoBehaviour, IController
+{
+    [SerializeField]
+    private float startingHealth;
+    [SerializeField]
+    private Image healthBar;
+
+    private float currentHealth;
+
+    public UniTask Prepare()
+    {
+        currentHealth = startingHealth;
+        healthBar.fillAmount = 1f;
+        return UniTask.CompletedTask;
+    }
+
+    public void ReceiveDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        healthBar.fillAmount = currentHealth / startingHealth;
+    }
+
+    public UniTask Setup()
+    {
+        return UniTask.CompletedTask;
+    }
+}
